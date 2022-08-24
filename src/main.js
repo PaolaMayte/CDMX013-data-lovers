fe9019e03e45f6afab45deafdb5848177b6b75fd
PaolaMayte <109124235+PaolaMayte@users.noreply.github.com>
2022-08-23 22:03:27 -0500

    agrega imagen y nombre de cada pokemon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import allData from './data/pokemon/pokemon.js'
+const root = document.getElementById ('root')
 
-console.log(example, data);
+const pokemons=allData.pokemon
+// data.pokemon.forEach(element =>{
+// const card = `<div class="card">
+// <img src="25.png">
+// <h3>nombre</h3> 
+// </div>`
+// main.append (card)
+// })
+
+// crear tarjeta
+
+const generadorHTML=(pokemon)=>{
+    const div = document.createElement('div')
+
+    const img = document.createElement('img')
+    img.setAttribute('src',pokemon.img)
+
+    const name = document.createElement('h3')
+    name.textContent = pokemon.name
+    
+    div.append(img,name)
+    return div  
+}
+
+pokemons.forEach(onePokemon=>root.appendChild(generadorHTML(onePokemon)))
