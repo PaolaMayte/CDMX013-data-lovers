635a6e2327d371a4c152582e313c71b04825f9ed
PaolaMayte <109124235+PaolaMayte@users.noreply.github.com>
2022-09-20 11:03:06 -0500

    código final

--- src/main.js
+++ src/main.js
@@ -1,89 +1,103 @@
 //importando data de otros archivos
+//import pokemon from './data/pokemon/pokemon.js'
 import allData from './data/pokemon/pokemon.js'
-import {regionFilter, typeFilter, sortAZ} from './data.js'
+import { regionFilter, typeFilter, sortAZ, sortByNum, combatPokemon } from './data.js'
 
 //declarando la constante del elemento donde de van a imprimir las tajetas en nuestro HTML
-const root = document.getElementById ('root')
+const root = document.getElementById('root')
 root.classList = 'displayStyle' //añade formato al elemento para mostrarlo en grid
-
-const pokemonsData=allData.pokemon //metiendo la data en una constante para nuestras funciones
+const pokemonsData = allData.pokemon //metiendo la data en una constante para nuestras funciones
 
 // declarando una constante que contenga nuestra función para crear tarjetas e imprimirlas en pantalla
-const generadorHTML=(pokemon)=>{
+const generadorHTML = (pokemon) => {
     const div = document.createElement('div') // declarando una constante que crea un div en el HTML
-    div.classList='card'
+    div.classList = 'card'
 
     const img = document.createElement('img')
-    img.setAttribute('src',pokemon.img)
-    img.classList= 'pokemon'
+    img.setAttribute('src', pokemon.img)
+    img.classList = 'pokemon'
 
     const num = document.createElement('h3')
     num.textContent = '#' + pokemon.num
-    num.classList='num'
+    num.classList = 'num'
 
     const name = document.createElement('h3')
     name.textContent = pokemon.name
     
-    div.append(num,img,name) //anexando las constantes creadas para el contenido de la tarjeta a l div que estamos creando
-    return div  
+
+    div.append(num, img, name) //anexando las constantes creadas para el contenido de la tarjeta a l div que estamos creando
+    return div
 }
 
 //convocando la función del generador de tarjetas para imprimir todos los pokemones contenidos en la data
-pokemonsData.forEach(onePokemon=>root.appendChild(generadorHTML(onePokemon)))
+pokemonsData.forEach(onePokemon => root.appendChild(generadorHTML(onePokemon)))
 
-
-const types= []
-const typeSelector= document.getElementById("type")
+//filtrando tipo y regiones adjuntándolos al selector de manera dinámica
+const types = []
+const typeSelector = document.getElementById("type")
 const regionSelector = document.getElementById('region')
-pokemonsData.forEach(onePokemon=>{
-    onePokemon.type.forEach(pokemonType=> types.push(pokemonType))
+
+pokemonsData.forEach(onePokemon => {
+    onePokemon.type.forEach(pokemonType => types.push(pokemonType))
 })
 let unique = [...new Set(types)]; // eliminando los elementos repetidos del array de tipos
-unique.forEach (oneType=> {
+unique.forEach(oneType => {
     const typeOption = document.createElement('option')
     typeOption.textContent = oneType
-    typeOption.value=oneType
-    typeSelector.append(typeOption)})
-
-// typeSelector.addEventListener('click', typesFilter);
-// function typesFilter(){
-//     pokemonsData.filter()
-// }
-
-// FILTRO TYPES
-const pokeTypes = typeFilter(pokemonsData, types)
-console.log('poyo', pokeTypes)
-
-typeSelector.addEventListener('change',(event)=>{
-    root.innerHTML=''
-typeFilter (pokemonsData,event.target.value).forEach(onePokemon=>root.appendChild(generadorHTML(onePokemon)))
-    console.log (typeFilter (pokemonsData,event.target.value))
+    typeOption.value = oneType
+    typeSelector.append(typeOption)
 })
 
+typeSelector.addEventListener('change', (event) => {
+    root.innerHTML = ''
+    regionSelector.selectedIndex = 0
+    const pokemonType = event.target.value
+    const filteredPokemons = typeFilter(pokemonsData, pokemonType)
+    const numberPokemons = filteredPokemons.length
+    const totalPokemons = pokemonsData.length
+    const pokePercentage = ((numberPokemons * 100) / totalPokemons).toFixed(2)
+    const typePercent=document.createElement('div')
+    typePercent.classList = 'text'
+    typePercent.textContent= 'These pokemon type accounts for ' + pokePercentage + '% of all Pokemon!'
+    root.appendChild(typePercent)
+    typeFilter(pokemonsData, event.target.value).forEach(onePokemon => root.appendChild(generadorHTML(onePokemon)))
+    
+})
 
-
-// pokemonsData.forEach(onePokemon=>{
-//     pokemonsData.region.forEach(pokemonRegion=> region.push(pokemonRegion))
-//     console.log(region)
-// })
-
-
-regionSelector.addEventListener('change', (event)=>{
-    root.innerHTML=''
+regionSelector.addEventListener('change', (event) => {
+    root.innerHTML = ''
     typeSelector.selectedIndex = 0
-    regionFilter (pokemonsData, event.target.value).forEach(onePokemon=>root.appendChild(generadorHTML(onePokemon)))
+    regionFilter(pokemonsData, event.target.value).forEach(onePokemon => root.appendChild(generadorHTML(onePokemon)))
 })
 
+//función para ordenado
 const sortSelector = document.getElementById('sort')
-sortSelector.addEventListener('change', (event)=>{
+sortSelector.addEventListener('change', (event) => {
     const value = event.target.value
-    const sortedItems = sortAZ(pokemonsData, event.target)
-    root.innerHTML=''
-    if (value === 'az'){
-        sortedItems.forEach(onePokemon => root.appendChild(generadorHTML(onePokemon)))
+    root.innerHTML = ''
+    if (value === 'number') {
+        const sortedNumbers = sortByNum(pokemonsData)
+        sortedNumbers.forEach(onePokemon => root.appendChild(generadorHTML(onePokemon)))
+    } else {
+        const sortedItems = sortAZ(pokemonsData)
+        if (value === 'az') {
+            sortedItems.forEach(onePokemon => root.appendChild(generadorHTML(onePokemon)))
+        }
+        if (value === 'za') {
+            sortedItems.reverse().forEach(onePokemon => root.appendChild(generadorHTML(onePokemon)))
+        }
     }
-    if (value === 'za'){
-        sortedItems.reverse().forEach(onePokemon => root.appendChild(generadorHTML(onePokemon)))
-    }    
+})
+
+const statsButton = document.getElementById('stats');
+statsButton.addEventListener('click', () => {
+    root.innerHTML = ''
+    const strongest = document.createElement('div')
+    strongest.classList = 'text'
+    const top10 = combatPokemon(pokemonsData).slice(0, 9)
+    strongest.textContent = 'Below we show you the 10 strongest pokemon!'
+    root.appendChild(strongest)
+    top10.forEach(onePokemon => root.appendChild(generadorHTML(onePokemon)))
+    
 })
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,8 +1,8 @@
-import { typeFilter, regionFilter } from '../src/data.js';
+import { typeFilter, regionFilter, sortAZ, sortByNum, combatPokemon } from '../src/data.js';
 import allData from '../src/data/pokemon/pokemon.js';
 
 describe('type filters', () => {
-  it('is a filter by water type', ()=>{
+  it('is a filter by water type', () => {
     //given user wants to filter pokemon by water type
     //when they select the water type option
     //then water type pokemon will appear
@@ -13,9 +13,36 @@ describe('type filters', () => {
 })
 
 describe('region filters', () => {
-  it('is a filter by region', ()=>{
+  it('is a filter by region', () => {
     const generation = 'kanto';
     const resultGeneration = regionFilter(allData.pokemon, generation)
     expect(resultGeneration.length).toBe(151)
+  })
+})
+
+describe('sortAZ', () => {
+  it('is an alphabetical sorting function', () => {
+    const pokemonsArray = [{name:'daniel'},{name:'camilo'}, {name:'aline'}];
+    const sortedArray = [{name:'aline'}, {name:'camilo'}, {name:'daniel'}];
+    expect(sortAZ(pokemonsArray)).toEqual(sortedArray);
+  })
+})
+
+describe('sortByNum', () => {
+  it('is a sorting function', () => {
+    const pokemonsArray = [{num:'7'},{num:'9'}, {num:'2'}];
+    const sortedArray = [{num:'2'}, {num:'7'}, {num:'9'}];
+    expect(sortByNum(pokemonsArray)).toEqual(sortedArray);
+  })
+})
+
+describe('combatPokemon', () => {
+  it('is a sorting function', () => {
+    const pokemonsArray = [{name:'pikachu', stats:{"max-cp": "1261"}},{name:'mewtwo', stats:{"max-cp": "4178"}}, {name:'magikarp', stats:{"max-cp": "274"}}];
+    const stats= [{ name: 'mewtwo', stats: { 'max-cp': '4178' } },
+      { name: 'pikachu', stats: { 'max-cp': '1261' } },
+      { name: 'magikarp', stats: { 'max-cp': '274' } }
+    ]
+    expect(combatPokemon(pokemonsArray)).toEqual(stats);
   })
 })
