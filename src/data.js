2aaccd81d131acd297b12ac15e28b3bb00820912
PaolaMayte <109124235+PaolaMayte@users.noreply.github.com>
2022-09-20 11:50:26 -0500

    Experimentos (#14)
    * formato de selectores
    
    * funcion filtrar en consola
    
    * filtrar
    
    * esbozo filtro dos
    
    * segundo-filtro-terminado
    
    * sort
    
    * agrega los test
    
    * código final

--- src/data.js
+++ src/data.js
@@ -1,32 +1,49 @@
-function typeFilter (pokemons, type){
-    const typesResult = pokemons.filter( (pokemon)=>{
-        if (pokemon.type.includes(type)){
-        return true
+function typeFilter(pokemons, type) {
+    const typesResult = pokemons.filter((pokemon) => {
+        if (pokemon.type.includes(type)) {
+            return true
         }
         else {
-        return false
+            return false
         }
     })
     return typesResult
 }
 
-function regionFilter (pokemons, region){
+function regionFilter(pokemons, region) {
     // console.log(pokemons)
     // console.log(region)
-    
- 
-    const regionResult = pokemons.filter( (pokemon)=>{
-       return pokemon.generation.name===region
+
+
+    const regionResult = pokemons.filter((pokemon) => {
+        return pokemon.generation.name === region
     })
-    console.log (regionResult)
+    //console.log (regionResult)
     return regionResult
 }
 
-function sortAZ (pokemons){
-    const pokeNames = pokemons.sort((onePokemon, twoPokemon)=>{
+function sortAZ(pokemons) {
+    const pokeNames = pokemons.sort((onePokemon, twoPokemon) => {
         return onePokemon.name.localeCompare(twoPokemon.name)
     })
-return pokeNames
+    return pokeNames
 }
 
-export {typeFilter, regionFilter, sortAZ}
+function sortByNum(pokemons) {
+    const pokeNumbers = pokemons.sort((onePokemon, twoPokemon) => {
+        return onePokemon.num.localeCompare(twoPokemon.num)
+    })
+    return pokeNumbers
+}
+
+function combatPokemon(pokemons) {
+    const maxCP = pokemons.sort(function (onePokemon, twoPokemon) {
+        const maxCP1 = parseInt(onePokemon.stats['max-cp']);
+        const maxCP2 = parseInt(twoPokemon.stats['max-cp']);
+        return maxCP2 - maxCP1
+    })
+    return maxCP
+}
+
+
+export { typeFilter, regionFilter, sortAZ, sortByNum, combatPokemon }
